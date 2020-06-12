901537cf03a29e992d86b4a97987cb8db6cc9246
Kirill N <twillyz@icloud.com>
2020-06-12 19:08:04 +0700

    global styles and routes

---
+++ src/core/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from '../../screens/Home/Home';
+import { Recipes } from '../../screens/Recipes/Recipes';
+
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/recipes">
+        <Recipes />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+);
---
+++ src/core/styles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 1.6;
+  color: #222;
+  background: #eee;
+  min-height: 100vh;
+}`;
