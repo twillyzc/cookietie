ff2a83cab51d8cba3bb1e1445415ef8e0140c08d
Kirill N <twillyz@icloud.com>
2020-06-23 00:28:44 +0700

    fix global styles

--- src/core/styles/global.ts
+++ src/core/styles/global.ts
@@ -1,21 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-}
-
 *,
 *::before,
 *::after {
+  margin: 0;
+  padding: 0;
   box-sizing: inherit;
 }
 
 html {
   box-sizing: border-box;
   font-size: 62.5%;
-
 }
 
 body {
